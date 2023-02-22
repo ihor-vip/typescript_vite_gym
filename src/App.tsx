0b548a279a4be5034a6158d1a63bb078a4cbf909
ihor-vip <vip20140902@gmail.com>
2023-02-22 09:58:05 +0200

    create separate file for enum

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import Navbar from '@/scenes/navbar';
 import {useState} from "react";
 
-enum SelectedPage {
-    Home = 'home',
-    Benefits = 'benefits',
-    OurClasses = 'ourclasses',
-    ContactUs = 'contactus'
-}
+
 
 function App() {
     const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
